458fbe5e924caf32f21e9f8b4eadc78fa5a5f774
NurdauletKasymbek <kasymbeknur502@gmail.com>
2025-07-19 10:59:07 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+
+
+BOT_TOKEN = "7983931676:AAG6Oq9hAr6IDAwaFuyFaQRpVUKfWPHdBeE"
+SHEET_ID = "1-p3zTUrVaAeJ7TJHkNytzRkWkxrInaPOaw_42DVc8IE"
+DRIVERS_SHEET = "Drivers"
+REQUESTS_SHEET = "Requests"
+DRIVER_GROUP_ID = -1002832319912  # Жүргізушілер тобының Telegram ID (теріс санмен)
